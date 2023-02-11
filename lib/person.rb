60f042d785b28f4dbceb9989dee7f99f0ff66970
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-11 03:00:08 -0500

    commit tree 683d3a967f1ea978a9c843d24133cc41ee9e6069

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,71 @@
 # your code goes here
+class Person
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def happiness=(num)
+        @happiness = if num > 10
+            10
+        elsif num < 0
+            0
+        else
+            num
+        end
+    end
+
+    def hygiene=(num)
+        @hygiene = num.clamp(0, 10)
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def get_paid amount
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend friend
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
